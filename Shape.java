acffdd6fb7bdd0fcc6a1752f6af1032829bbbc63
Miguel A Rodriguez <miguelrdrgz47@gmail.com>
2015-10-06 19:27:21 -0700

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,28 @@
+//******************************************************************************
+//  Title:  Shape.java       
+//  Author: Miguel A Rodriguez
+//  Date:   1/1/2015
+//  Course: CSC 112-001
+//******************************************************************************
+//  Description:
+//****************
+//            This is an abstract class containing two methods shared by most of the classes.
+//         
+// 
+//******************************************************************************
+   
+   
+//         UML Diagram for this helper class         
+//          ***********************************************************************************     
+//Class:      Shape
+//          ***********************************************************************************
+//Methods:
+//            +computeArea(): double
+//            +computePerimeter(): double
+//          ************************************************************************************* 
+
+public abstract class Shape
+{
+   abstract public double computeArea();
+   abstract public double computePerimeter();
+}
