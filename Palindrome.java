bba899394c1b146518551bbc4ae53d409135c223
Miguel A Rodriguez <miguelrdrgz47@gmail.com>
2015-10-31 11:15:56 -0700

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,53 @@
+//********************************************************************************************
+//  Palindrome.java       Java Foundations
+//  Author: Miguel A Rodriguez
+//  Date: 10/31/2015
+//  Course: CSC 112-001
+//  Description:
+//           Creates a system using a stack and a queue to test whether a
+//           given string is a palindrome.
+//  
+//*********************************************************************************************
+//
+//   
+//         UML Diagram for main Application   
+//         ************************************************************************************  
+//           Application Class         
+//          ***********************************************************************************     
+//Class:      Palindrome
+//          ***********************************************************************************
+//Variables&Objects: 
+//            -message:   String
+//            -again:     String
+//            -string:    String
+//            -letter:    String 
+//            -letter2:   String
+//            -scan:      Scanner
+//            -result:    Character
+//            -result2:   Character
+//          ************************************************************************************
+//Methods:
+//            +main(args: String[] :void 
+//          ************************************************************************************* 
+
+//   ********************************************************************************************
+//       Program Logic:
+//              Create Scanner object, String and Character variables
+//              Create a Queue and a Stack, as an array or linked list
+///             Ask for and enter a message.
+//              Place the message into a queue and a stack
+//              Initialize a count = 0
+//              Remove the characters in the message from the queue and the stack, 
+///             When complete, compare the count to the length of the message.
+//              If they are the same, the message is a palindrome.
+//              If not the message is not a palindrome.
+//              Repeat if the user wishes to do so 
+//         
+//**********************************************************************************************
+// Pseudocode 
+// 1. Read a string
+// 2. Place the message into a queue and a stack
+// 3. Remove the characters in the message from the queue and the stack
+// 4. compare results and prints out accordingly: whether is a palindrome or not
+// 5. Allows the user to continue
+//**********************************************************************************************
